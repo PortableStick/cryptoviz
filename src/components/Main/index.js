6fc0d4c18e805082dba454b7316a388befeb0f44
PortableStick <crashingwaves@fea.st>
2018-04-24 18:34:17 -0400

    Add specialized component to router

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
 import Summary from '../Summary';
+import Specialized from '../Specialized';
 
 function Main() {
   return (
     <main>
       <Switch>
         <Route exact path="/" component={Summary} />
+        <Route path="/:name" component={Specialized} />
       </Switch>
     </main>
   );
