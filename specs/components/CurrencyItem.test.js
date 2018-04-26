25dfc4471b15070cccb0a4265d51fd5a5978722f
PortableStick <crashingwaves@fea.st>
2018-04-26 10:50:41 -0400

    Fix test

--- specs/components/CurrencyItem.test.js
+++ specs/components/CurrencyItem.test.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import CurrencyItem from '../../src/components/CurrencyItem';
+import React from "react";
+import { shallow } from "enzyme";
+import CurrencyItem from "../../src/components/CurrencyItem";
 
-const props =
-    {
-      cap24hrChange: 3.8,
-      long: 'Bitcoin',
-      mktcap: 155456934730.3,
-      perc: 3.8,
-      price: 9146.9,
-      shapeshift: true,
-      short: 'BTC',
-      supply: 16995587,
-      usdVolume: 7508110000,
-      volume: 7508110000,
-      vwapData: 8898.069574442592,
-      vwapDataBTC: 8898.069574442592,
-    };
+const props = {
+  cap24hrChange: 3.8,
+  long: "Bitcoin",
+  mktcap: 155456934730.3,
+  perc: 3.8,
+  price: 9146.9,
+  shapeshift: true,
+  short: "BTC",
+  supply: 16995587,
+  usdVolume: 7508110000,
+  volume: 7508110000,
+  vwapData: 8898.069574442592,
+  vwapDataBTC: 8898.069574442592,
+  rank: 1
+};
 
-describe('<CurrencyItem />', () => {
+describe("<CurrencyItem />", () => {
   const component = shallow(<CurrencyItem {...props} />);
 
-  it('should render', () => {
+  it("should render", () => {
     expect(component.exists()).toEqual(true);
   });
 });
